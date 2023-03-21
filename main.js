d2fbec478bb059ec3384ffe13d72b63522560175
Christian Martinez <chmartinez.cba@gmail.com>
2023-03-21 20:33:50 -0300

    add: shiny toggle

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
-/* 
-const createPokemonData = (data) => {
-  const pokemon = {
-    name: data.name,
-    id: data.id,
-
-    hp: data.stats[0].base_stat,
-    atk: data.stats[1].base_stat,
-    def: data.stats[2].base_stat,
-    atk_spc: data.stats[3].base_stat,
-    def_spc: data.stats[4].base_stat,
-    speed: data.stats[5].base_stat,
-
-    sprite: {
-      front: data.sprites.front_default,
-      back: data.sprites.front_default,
-      front_shiny: data.sprites.front_shiny,
-      back_shiny: data.sprites.back_shiny,
-    },
-
-    type:[data.types[0].type.name, data.types[0]?.type.name]
-  }
-  return pokemon
-} */
-
-
-const template_data = document.getElementById('template_data').content
 const main = document.getElementById('main')
 const sprite_container = document.getElementById('sprite_container')
+const template_data = document.getElementById('template_data').content
+const btn_shiny = document.getElementById('btn_shiny')
 
+const limit = 500
 let pokeData = {
   pokemon: undefined,
   shiny: false
 }
-
-//pokemon data
 
 // Funciones
 
@@ -47,35 +21,44 @@ const renderCard = (pokeData) => {
   const fragment = document.createDocumentFragment()
   const clone = template_data.cloneNode(true)
   
-
   if (pokeData.shiny) {
     clone.querySelector('#sprite').setAttribute('src',pokeData.pokemon.sprites.other["official-artwork"].front_shiny)
   }else{
     clone.querySelector('#sprite').setAttribute('src',pokeData.pokemon.sprites.other["official-artwork"].front_default)
   }
 
+  clone.querySelector('#poke_id').textContent = `#${pokeData.pokemon.id}`
   clone.querySelector('#name').textContent = pokeData.pokemon.name
-  clone.querySelector('#nro').textContent = '#'+pokeData.pokemon.id
   clone.querySelector('#type_1').textContent = `${pokeData.pokemon.types[0].type.name}`
   clone.querySelector('#type_1').classList.add(`${pokeData.pokemon.types[0].type.name}-bg`)
   clone.querySelector('#card').classList.add(`${pokeData.pokemon.types[0].type.name}-bg`)
-  /* clone.querySelectorAll('.stat_name').classList.add(`${pokeData.pokemon.types[0].type.name}-font`) */
+
+  /* Carga de tags de types y de background */
   if (pokeData.pokemon.types[1]?.type.name == undefined) {
     clone.querySelector('#type_2').classList.add('type-hide')
   }else{
     clone.querySelector('#type_2').textContent = `${pokeData.pokemon.types[1]?.type.name}`
     clone.querySelector('#type_2').classList.add(`${pokeData.pokemon.types[1]?.type.name}-bg`)
   }
+
+  /* Carga de stats */
   clone.querySelector('#hp_value').textContent = pokeData.pokemon.stats[0].base_stat
   clone.querySelector('#atk_value').textContent = pokeData.pokemon.stats[1].base_stat
   clone.querySelector('#def_value').textContent = pokeData.pokemon.stats[2].base_stat
-  clone.querySelector('#atk_spc_value').textContent = pokeData.pokemon.stats[3].base_stat
-  clone.querySelector('#def_spc_value').textContent = pokeData.pokemon.stats[4].base_stat
-  clone.querySelector('#speed_value').textContent = pokeData.pokemon.stats[5].base_stat
+  clone.querySelector('#satk_value').textContent = pokeData.pokemon.stats[3].base_stat
+  clone.querySelector('#sdef_value').textContent = pokeData.pokemon.stats[4].base_stat
+  clone.querySelector('#spd_value').textContent = pokeData.pokemon.stats[5].base_stat
+  
+  /* Carga status bar de stats*/
+  clone.querySelector('#hp_progress').setAttribute('value',pokeData.pokemon.stats[0].base_stat)
+  clone.querySelector('#atk_progress').setAttribute('value',pokeData.pokemon.stats[1].base_stat)
+  clone.querySelector('#def_progress').setAttribute('value',pokeData.pokemon.stats[2].base_stat)
+  clone.querySelector('#satk_progress').setAttribute('value',pokeData.pokemon.stats[3].base_stat)
+  clone.querySelector('#sdef_progress').setAttribute('value',pokeData.pokemon.stats[4].base_stat)
+  clone.querySelector('#spd_progress').setAttribute('value',pokeData.pokemon.stats[5].base_stat)
 
   fragment.appendChild(clone)
-  const nodo = main.children[0]
-  main.replaceChild(fragment,nodo)
+  main.replaceChild(fragment,main.children[1])
 }
 
 const fetchData = async (nro) => {
@@ -91,12 +74,11 @@ const fetchData = async (nro) => {
 // Eventos
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetchData(getRandomInt(1,152))
+  fetchData(getRandomInt(1,limit))
 })
 
-
 btn_random.addEventListener('click', () => {
-  fetchData(getRandomInt(1,152))
+  fetchData(getRandomInt(1,limit))
 })
 
 btn_prev.addEventListener('click',() => {
@@ -106,8 +88,12 @@ btn_prev.addEventListener('click',() => {
 })
 
 btn_next.addEventListener('click',() => {
-  if (pokeData.pokemon.id < 150) {
+  if (pokeData.pokemon.id < limit) {
     fetchData(pokeData.pokemon.id+1)
   }
 })
 
+btn_shiny.addEventListener('click', () => {
+  pokeData.shiny ? pokeData.shiny = false : pokeData.shiny = true
+  fetchData(pokeData.pokemon.id)
+})
